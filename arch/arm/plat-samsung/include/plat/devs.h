7e106d3b7fffc79f567bf49fb704aee173313d47
Pau Baiget <pau.baiget@arm.com>
2013-12-05 17:17:37 +0100

    mali: ARM Mali driver kernel trunk sync [revision 97160 (Lemon)]
    Change-Id: I222f58b120ce12589ebcb9903eebaa0d8e93688a

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -168,7 +168,7 @@ extern struct platform_device samsung_asoc_idma;
 extern struct platform_device samsung_device_keypad;
 
 extern struct platform_device s5p_device_fimg2d;
-#ifdef CONFIG_MALI_T6XX
+#ifdef CONFIG_MALI_MIDGARD
 extern struct platform_device exynos5_device_g3d;
 #endif
 /* s3c2440 specific devices */
